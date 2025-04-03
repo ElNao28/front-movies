e42ac564db4322e2ff78ca393192b7e877441939
Luis Jair <naopubg28@gmail.com>
2025-04-03 15:12:22 -0600

    create files structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admon',
+    loadChildren: () =>
+      import('./Modules/admon/admon.module').then((m) => m.AdmonModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
