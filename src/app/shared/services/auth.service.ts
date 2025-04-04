725c06d9d16df262ccd5cd7c2904949e4c2c8bad
luis jair <jair.escalante@phs.mx>
2025-04-04 12:57:55 -0600

    add enviroments and create service to login users

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+  private urlApi: string = environment.urlApi;
+
+  public authUser(userData: any): Observable<Auth> {
+    return this.http.post<Auth>(`${this.urlApi}auth`, userData);
+  }
+}
