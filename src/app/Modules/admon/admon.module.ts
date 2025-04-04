c32c234feff33e50f28a174dace22c99f5bbd1a3
luis jair <jair.escalante@phs.mx>
2025-04-04 12:49:24 -0600

    move login component to public directory

--- src/app/Modules/admon/admon.module.ts
+++ src/app/Modules/admon/admon.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdmonRoutingModule } from './admon-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    AdmonRoutingModule
-  ]
+  declarations: [],
+  imports: [CommonModule, AdmonRoutingModule],
 })
-export class AdmonModule { }
+export class AdmonModule {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'public',
+    loadChildren: () =>
+      import('./Modules/public/public.module').then((m) => m.PublicModule),
+  },
+  {
     path: 'admon',
     loadChildren: () =>
       import('./Modules/admon/admon.module').then((m) => m.AdmonModule),
+  },
+  {
+    path: '',
+    redirectTo: 'public',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'public',
+    pathMatch: 'full',
   },
 ];
 
