81de69ae23d7f2605fd19cbc6edce2bc17273cac
luis jair <jair.escalante@phs.mx>
2025-04-04 19:28:31 -0600

    create desing to register form

--- src/app/Modules/public/public.module.ts
+++ src/app/Modules/public/public.module.ts
@@ -5,9 +5,10 @@ import { PublicRoutingModule } from './public-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     PublicRoutingModule,
