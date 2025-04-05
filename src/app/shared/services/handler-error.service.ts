b227739fada74fd7b912f8c456473364a0069d91
luis jair <jair.escalante@phs.mx>
2025-04-04 21:26:10 -0600

    chage messages to handler error service and add submit event to register component

--- src/app/shared/services/handler-error.service.ts
+++ src/app/shared/services/handler-error.service.ts
@@ -7,27 +7,28 @@ import { MessageService } from 'primeng/api';
 export class HandlerErrorService {
   constructor(private messageService: MessageService) {}
   public handlerError(err: any) {
-    const statusCode: number = err.status;
-    switch (statusCode) {
-      case 404:
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'Usuario no registrado.',
-        });
-        break;
-      case 401:
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'Contraseña incorrecta.',
-        });
-        break;
-      default:
+    console.log(err);
+    const { status, error } = err;
+    switch (status) {
+      case 500:
         this.messageService.add({
           severity: 'error',
           summary: 'Error',
           detail: 'Ha ocurrido un error.',
+        });
+        break;
+      default:
+        if (!status) {
+          return this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Ha ocurrido un error.',
+          });
+        }
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: error.message,
         });
         break;
     }
