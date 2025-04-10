dec4367c414b4578141f9631da0f060930d91004
luis jair <jair.escalante@phs.mx>
2025-04-09 19:39:40 -0600

    add slider

--- src/app/Modules/public/pages/home/home.component.ts
+++ src/app/Modules/public/pages/home/home.component.ts
@@ -4,8 +4,47 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  private currentIndex: number = 0;
+  public images: { title: string; url: string }[] = [
+    {
+      title: 'Saikyou no Ousama, Nidome no Jinsei wa Nani wo Suru?',
+      url: 'slider/1.png',
+    },
+    {
+      title: 'Saikyou no Ousama, Nidome no Jinsei wa Nani wo Suru?',
+      url: 'slider/2.jpg',
+    },
+    {
+      title: 'Saikyou no Ousama, Nidome no Jinsei wa Nani wo Suru?',
+      url: 'slider/3.png',
+    },
+    {
+      title: 'Saikyou no Ousama, Nidome no Jinsei wa Nani wo Suru?',
+      url: 'slider/4.png',
+    },
+    {
+      title: 'Saikyou no Ousama, Nidome no Jinsei wa Nani wo Suru?',
+      url: 'slider/5.jpg',
+    },
+  ];
+  public nextImage(): void {
+    if (this.currentIndex + 1 < this.images.length) {
+      this.currentIndex++;
+      this.updatePosition();
+    }
+  }
 
+  public beforeImage(): void {
+    if (this.currentIndex >= 1) {
+      this.currentIndex--;
+      this.updatePosition();
+    }
+  }
+  private updatePosition(): void {
+    const element = document.getElementById('slider') as HTMLDivElement;
+    element.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+  }
 }
